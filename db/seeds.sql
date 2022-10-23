4c089e1921787e4cb94a73e34e2d4d4b5be104a0
Isaac Falcon <ice6490@gmail.com>
2022-10-23 15:58:47 -0500

    added gitignore for node modules, added schema,seeds sql files with new database and tables and some info populated

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (id, name)
+VALUES (12, 'Walter White'),
+       (23, 'Jesse Pinkman'),
+       (34, 'Gus Fringe'),
+       (45, 'Mike Urman'),
+       (56, 'Skinny Pete');
+
+INSERT INTO role (title, salary, department_id)
+VALUES (),
+       (),
+       (),
+       (),
+       (),
+       (),
+       ();
+
+INSERT INTO employee ()
+VALUES (),
+       (),
+       (),
+       (),
+       ();
