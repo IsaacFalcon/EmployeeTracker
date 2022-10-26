b71ce79b20f51462fdc7f23297b2da06ac266c9a
Isaac Falcon <ice6490@gmail.com>
2022-10-26 17:49:38 -0500

    added fixes to database, added new functions for inquirer

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,16 +5,16 @@ VALUES ('Distribution'),
        ('Security'),
        ('Accounting');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (11, 'Lead Distributor', 400000),
-       (12, 'Sales Lead', 500000),
-       (13, 'Legal Advisor', 1000000),
-       (14, 'Security Specialist', 250000),
-       (15, 'Business Analyst', 300000);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Distributor', 400000, 1),
+       ('Sales Lead', 500000, 2),
+       ('Legal Advisor', 1000000, 3),
+       ('Security Specialist', 250000, 4),
+       ('Business Analyst', 300000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Walter', 'White', NULL),
-       ('Jesse', 'Pinkman', 1),
-       ('Saul', 'Goodman', 1),
-       ('Mike', 'Ehrmantraut', 1),
-       ('Lydia', 'Rodarte', 1);
+VALUES ('Walter', 'White', 1, NULL),
+       ('Jesse', 'Pinkman', 2, 1),
+       ('Saul', 'Goodman', 3, 1),
+       ('Mike', 'Ehrmantraut', 4, 1),
+       ('Lydia', 'Rodarte', 5, 1);
--- server.js
+++ server.js
@@ -2,25 +2,31 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
 
-    port: 3001,
+    port: 3306,
 
-    user: '',
+    user: process.env.DB_USER,
 
     // create environment variable
-    password: '', 
+    password: process.env.DB_PASSWORD, 
 
-    database: 'manager_db'
+    database: process.env.DB_NAME
+});
+connection.connect(function(err){
+    if (err) throw err;
+    mainPrompt();
 });
 
 function mainPrompt() {
+    console.log('Hello welcome to the employee team manager!');
     inquirer.prompt({
       type: 'list',
       name: 'task',
-      massage: 'What would you like to do?',
+      message: 'What would you like to do?',
       choices: [
         'View All Departments',
         'View All Roles',
@@ -29,47 +35,144 @@ function mainPrompt() {
         'Add a Role',
         'Add an Employee',
         'Update an Employee Role',
-        'Update Employee Managers',
+        'Delete Employee',
         'DONE']
+    }).then(function(response){
+        if(response.task === 'View All Departments') {
+            viewDep();
+        } else if(response.task === 'View All Roles') {
+            viewRoles();
+        } else if(response.task === 'View All Employees') {
+            viewEmpl();
+        } else if(response.task === 'Add a department') {
+            addDep();
+        } else if(response.task === 'Add a Role') {
+            addRole();
+        } else if(response.task === 'Add an Employee') {
+            addEmpl();
+        } else if(response.task === 'Update an Employee Role') {
+            updateRole();
+        } else if(response.task === 'Delete Employee') {
+            deleteEmpl();
+        } else if(response.task === 'DONE') {
+            console.log('Goodbye');
+            connection.end();
+        }
     });
 };
 
 function viewDep() {
    console.log('viewing all departments');
-   
+   connection.query('SELECT * FROM department', (err, res) => {
+    if(err) throw err;
+    console.table(res);
+    mainPrompt();
+   })
 };
 
 function viewRoles() {
     console.log('viewing all roles');
-
+    connection.query('SELECT * FROM role', (err, res) => {
+    if(err) throw err;
+    console.table(res);
+    mainPrompt();
+    })
 };
 
 function viewEmpl() {
     console.log('viewing all employees');
-
+    connection.query('SELECT * FROM employee', (err, res) => {
+    if(err) throw err;
+    console.table(res);
+    mainPrompt();
+    })
 };
 
 function addDep() {
     console.log('adding department');
+    connection.query('SELECT * FROM department', (err, res) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'depName',
+            message: 'What is the new departments name?'
+        }
+    ]).then(data => {
+        let depName = res.find(department => department.name === data.depName)
+        connection.query('insert into department set ?', {
+           name: data.depName 
+        });
+        mainPrompt();
+    })
+})};
 
-};
-
-function AddRole() {
+function addRole() {
     console.log('adding role');
 
 };
 
-function AddEmpl() {
+function addEmpl() {
     console.log('adding employee');
-
+    connection.query('SELECT * FROM role', (err, res) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the new employees first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the new employees last name?'
+            },
+            {
+                type: 'list',
+                name: 'roleTitle',
+                message: 'Please Select a role for the new employee',
+                choices: res.map(role => role.title)
+            }
+        ]).then(data => {
+           let roleTitle = res.find(role => role.title === data.roleTitle)
+           connection.query('insert into employee set ?', {
+            first_name: data.firstName,
+            last_name: data.lastName,
+            role_id: roleTitle.id,
+            manager_id: 1
+           });
+           mainPrompt();
+        })
+    }); 
+    
 };
 
 function updateRole() {
-    console.log('updating role');
 
 };
 
-function updateManager() {
-    console.log('updating manager');
-
+function deleteEmpl() {
+    console.log('deleting employee');
+    connection.query('SELECT * FROM role', (err, res) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employees first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employees last name?'
+            }
+        ]).then(function(data) {
+        connection.query('DELETE FROM employee WHERE first_name = ? and last_name = ?', [
+            data.firstName,
+            data.lastName,
+            ],
+             function (err) {
+                if (err) throw err;
+            console.log(`\n ${data.firstName} ${data.lastName} has been deleted from the database... \n`)
+            })
+            mainPrompt();
+        })
+    })
 };
